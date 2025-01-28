c2ea5cc32f8c8017e189dbbcb87fd8920f9800c7
tmckell1 <taylormckell@churchofjesuschrist.org>
2025-01-28 14:13:13 -0700

    yep

--- puzzle_blocks_script_2.js
+++ puzzle_blocks_script_2.js
@@ -72,87 +72,87 @@ function initializeGame() {
     initializeLevel(currentLevelIndex);
 }
 
-// When level is completed, move to next level
-function checkWinCondition() {
-    let blocksOnTarget = 0;
-    blocks.forEach(block => {
-        if (isOnTarget(block.x, block.y)) blocksOnTarget++;
-    });
-    if (blocksOnTarget === blocks.length) {
-        currentLevelIndex = (currentLevelIndex + 1) % LEVELS.length;
-        initializeLevel(currentLevelIndex);
-    }
-}
+// // When level is completed, move to next level
+// function checkWinCondition() {
+//     let blocksOnTarget = 0;
+//     blocks.forEach(block => {
+//         if (isOnTarget(block.x, block.y)) blocksOnTarget++;
+//     });
+//     if (blocksOnTarget === blocks.length) {
+//         currentLevelIndex = (currentLevelIndex + 1) % LEVELS.length;
+//         initializeLevel(currentLevelIndex);
+//     }
+// }
 
-// Reset current level
-function resetLevel() {
-    initializeLevel(currentLevelIndex);
-}
+// // Reset current level
+// function resetLevel() {
+//     initializeLevel(currentLevelIndex);
+// }
 
-// Render the game
-function renderGame() {
-    const gameGrid = document.getElementById('gameGrid');
-    gameGrid.innerHTML = '';
-    for (let y = 0; y < currentLevel.length; y++) {
-        for (let x = 0; x < currentLevel[y].length; x++) {
-            const cell = document.createElement('div');
-            cell.className = 'cell';
-            if (currentLevel[y][x] === WALL) cell.classList.add('wall');
-            else if (x === playerPos.x && y === playerPos.y) cell.classList.add('player');
-            else if (currentLevel[y][x] === BLOCK) {
-                cell.classList.add('block');
-                if (isOnTarget(x, y)) cell.classList.add('on-target');
-            } else if (currentLevel[y][x] === TARGET) cell.classList.add('target');
-            gameGrid.appendChild(cell);
-        }
-    }
-    checkWinCondition();
-}
+// // Render the game
+// function renderGame() {
+//     const gameGrid = document.getElementById('gameGrid');
+//     gameGrid.innerHTML = '';
+//     for (let y = 0; y < currentLevel.length; y++) {
+//         for (let x = 0; x < currentLevel[y].length; x++) {
+//             const cell = document.createElement('div');
+//             cell.className = 'cell';
+//             if (currentLevel[y][x] === WALL) cell.classList.add('wall');
+//             else if (x === playerPos.x && y === playerPos.y) cell.classList.add('player');
+//             else if (currentLevel[y][x] === BLOCK) {
+//                 cell.classList.add('block');
+//                 if (isOnTarget(x, y)) cell.classList.add('on-target');
+//             } else if (currentLevel[y][x] === TARGET) cell.classList.add('target');
+//             gameGrid.appendChild(cell);
+//         }
+//     }
+//     checkWinCondition();
+// }
 
-// Initialize the game
-function initializeGame() {
-    blocks = [];
-    targets = [];
-    currentLevel = generateLevel();
-    renderGame();
-}
+// // Initialize the game
+// function initializeGame() {
+//     blocks = [];
+//     targets = [];
+//     currentLevel = generateLevel();
+//     renderGame();
+// }
 
-// Check if the player can move to a new position
-function canMove(newPos, movement) {
-    if (currentLevel[newPos.y][newPos.x] === WALL) return false;
-    if (currentLevel[newPos.y][newPos.x] === BLOCK) {
-        const beyondBlock = { x: newPos.x + movement.x, y: newPos.y + movement.y };
-        if (currentLevel[beyondBlock.y][beyondBlock.x] === WALL || 
-            currentLevel[beyondBlock.y][beyondBlock.x] === BLOCK) return false;
-        currentLevel[beyondBlock.y][beyondBlock.x] = BLOCK;
-        currentLevel[newPos.y][newPos.x] = EMPTY;
-        // Update block position in blocks array
-        const blockIndex = blocks.findIndex(b => b.x === newPos.x && b.y === newPos.y);
-        if (blockIndex !== -1) {
-            blocks[blockIndex] = {x: beyondBlock.x, y: beyondBlock.y};
-        }
-        return true;
-    }
-    return true;
-}
+// // Check if the player can move to a new position
+// function canMove(newPos, movement) {
+//     if (currentLevel[newPos.y][newPos.x] === WALL) return false;
+//     if (currentLevel[newPos.y][newPos.x] === BLOCK) {
+//         const beyondBlock = { x: newPos.x + movement.x, y: newPos.y + movement.y };
+//         if (currentLevel[beyondBlock.y][beyondBlock.x] === WALL || 
+//             currentLevel[beyondBlock.y][beyondBlock.x] === BLOCK) return false;
+//         currentLevel[beyondBlock.y][beyondBlock.x] = BLOCK;
+//         currentLevel[newPos.y][newPos.x] = EMPTY;
+//         // Update block position in blocks array
+//         const blockIndex = blocks.findIndex(b => b.x === newPos.x && b.y === newPos.y);
+//         if (blockIndex !== -1) {
+//             blocks[blockIndex] = {x: beyondBlock.x, y: beyondBlock.y};
+//         }
+//         return true;
+//     }
+//     return true;
+// }
 
-// Move the player
-function move(direction) {
-    const movements = {
-        up: { x: 0, y: -1 },
-        down: { x: 0, y: 1 },
-        left: { x: -1, y: 0 },
-        right: { x: 1, y: 0 }
-    };
-    const movement = movements[direction];
-    const newPos = { x: playerPos.x + movement.x, y: playerPos.y + movement.y };
-    if (canMove(newPos, movement)) {
-        currentLevel[playerPos.y][playerPos.x] = EMPTY;
-        playerPos = newPos;
-        currentLevel[playerPos.y][playerPos.x] = PLAYER;
-        renderGame();
-    }
-}
+// // Move the player
+// function move(direction) {
+//     const movements = {
+//         up: { x: 0, y: -1 },
+//         down: { x: 0, y: 1 },
+//         left: { x: -1, y: 0 },
+//         right: { x: 1, y: 0 }
+//     };
+//     const movement = movements[direction];
+//     const newPos = { x: playerPos.x + movement.x, y: playerPos.y + movement.y };
+//     if (canMove(newPos, movement)) {
+//         currentLevel[playerPos.y][playerPos.x] = EMPTY;
+//         playerPos = newPos;
+//         currentLevel[playerPos.y][playerPos.x] = PLAYER;
+//         renderGame();
+//     }
+// }
 
 // Initialize the game on window load
 window.onload = initializeGame;
